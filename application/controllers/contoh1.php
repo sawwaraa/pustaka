7bb9d1e73a55275be9f1c15460548a6d6c4f89d3
sawwaraa <sawwaraka@gmail.com>
2022-10-20 16:02:00 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rivaldlli Candiyansah
+            Saya tinggal di daerah bekasi
+            olah raga yang saya sukai adalah
+            Maen Catur";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rivaldlli</li>
+        <li>Nama Belakang : Candiyansah</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Nogosaren Kidul No.186, RT.6/RW.22, Kelurahan Nogotirto, Gamping</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cirebon</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Game</li>
+        <li>Playing football</li>
+    </ul>
+</section>
